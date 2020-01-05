eb016a9185863dbe71d484d84188e05557fce34b
rohit aggarwal <rohit.aggarwal@rohitaggarwal.net>
2020-01-05 12:04:18 +0530

    ada

--- src/main/java/com/example/hello/ApigatewayApplication.java
+++ src/main/java/com/example/hello/ApigatewayApplication.java
@@ -3,10 +3,15 @@ package com.example.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
+@EnableZuulProxy
+//Authorization
+//Authentication
+//logging
+//Monitoring
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/hello/logging/Logging.java
@@ -0,0 +1,46 @@
+package com.example.hello.logging;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class Logging extends ZuulFilter {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// Implement Business Logic
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		HttpServletRequest httpServletRequest = RequestContext.getCurrentContext().getRequest();
+		logger.info("Request ->{} request uri->{}",httpServletRequest,httpServletRequest.getRequestURI());
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		// Prem,Post,error request, free
+		return "pre";
+	}
+
+}
